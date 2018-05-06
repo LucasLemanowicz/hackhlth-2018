bc155e00fbeaa73b0d7e5ef004ec4f56f8e2b1ae
Lucas Lemanowicz <lemanowiczlucas@gmail.com>
2018-05-05 18:13:16 -0700

    Add Softheon Wallet API integration

--- app.py
+++ app.py
@@ -3,16 +3,24 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_ask import Ask, statement
+from softheon import SoftheonWalletAPI
 
 
 app = Flask(__name__)
 ask = Ask(app, '/ask/')
 CORS(app)
 
+# Initialize the Softheon Wallet for payment processing
+wallet = SoftheonWalletAPI(
+    os.environ.get('SOFTHEON_CLIENT_ID', ''),
+    os.environ.get('SOFTHEON_CLIENT_SECRET', '')
+)
+
 
 @app.route('/', methods=['GET'])
 def home():
     return 'Hello, world!'
+
 
 @ask.intent('HelloIntent')
 def hello(firstname):
@@ -20,8 +28,24 @@ def hello(firstname):
     return statement(text).simple_card('Hello', text)
 
 
+@ask.intent('PaymentIntent')
+def payment(payment_amount):
+
+    PAYMENT_FAILURE = 'Payment failed, please try again.'
+    PAYMENT_SUCCESS = 'Payment succeeded. Thank you.'
+
+    status, data = wallet.make_payment(payment_amount)
+    if not status:
+        return statement(PAYMENT_FAILURE)
+
+    success_card = 'Payment of ${} succeeded (Transaction ID: {})'.format(
+        data.get('paymentAmount', '0.00'),
+        data.get('result', []).get('merchantTransactionId', '422806694271965000')
+    )
+    return statement(PAYMENT_SUCCESS).simple_card(success_card)
+
+
 if __name__ == '__main__':
     app.run(debug=True,
             host='0.0.0.0',
             port=int(os.environ.get('PORT', 2020)))
-
---
+++ softheon.py
@@ -0,0 +1,86 @@
+import base64
+import json
+import os
+
+import requests
+#from softheon.wallet.api.client import SoftheonWalletAPI
+
+
+class SoftheonWalletAPI:
+    TOKEN_ENDPOINT = 'https://hack.softheon.io/oauth2/connect/token'
+    CREDITCARD_ENDPOINT = 'https://hack.softheon.io/api/payments/v1/creditcards'
+    PAYMENT_ENDPOINT = 'https://hack.softheon.io/api/payments/v1/payments'
+
+    BILLING_ADDRESS = {
+        'address1': '111 test',
+        'address2': '111 test',
+        'city': 'Stony Brook',
+        'state': 'NY',
+        'zipCode': '11111'
+    }
+
+    CREDIT_CARD = {
+        'cardNumber': '4134185779995000',
+        'securityCode': '123',
+        'expirationMonth': 3,
+        'expirationYear': 2018,
+        'cardHolderName': 'Test',
+        'billingAddress': BILLING_ADDRESS,
+        'email': 'example@example.com',
+    }
+
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token = self._get_access_token(client_id, client_secret)
+
+    def _get_access_token(self, client_id, client_secret):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        body = {
+            'scope': 'paymentapi',
+            'grant_type': 'client_credentials',
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+
+        http = requests.post(self.TOKEN_ENDPOINT, headers=headers, data=body)
+        resp = json.loads(http.text)
+
+        return resp['access_token']
+
+    def _get_credit_card_token(self):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + self.access_token
+        }
+
+        body = str(self.CREDIT_CARD)
+
+        http = requests.post(self.CREDITCARD_ENDPOINT, headers=headers,
+                             data=body)
+        resp = json.loads(http.text)
+
+        return resp['token']
+
+    def make_payment(self, payment_amount):
+        """ Make a payment to your provider """
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + self.access_token
+        }
+
+        body = str({
+            'paymentAmount': payment_amount,
+            'paymentMethod': {
+                'paymentToken': str(self._get_credit_card_token()),
+                'type': 'Credit Card'
+            }
+        })
+
+        http = requests.post(self.PAYMENT_ENDPOINT, headers=headers, data=body)
+        resp = json.loads(http.text)
+
+        return (resp.get('result', []).get('message', '') == 'Success', resp)
